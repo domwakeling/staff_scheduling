1aee65938a654bb83cc89b67369780981dfea20c
Dom Wakeling <domwakeling@gmail.com>
2022-11-04 21:47:14 +0000

    Further extend upload timeout

--- components/admin/transfer/TransferConfirmDialog.jsx
+++ components/admin/transfer/TransferConfirmDialog.jsx
@@ -16,7 +16,7 @@ const TransferConfirmDialog = (props) => {
             const res = await axios({
                 method: 'post',
                 url: '/api/schedule/regular/replace',
-                timeout: 10000,
+                timeout: 20000,
                 data: {
                     jsonData: data
                 }
