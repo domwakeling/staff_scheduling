aa4b7b2e6ee41879249d596b67b475a61cc741a4
Dom Wakeling <domwakeling@gmail.com>
2022-11-08 12:21:13 +0000

    Reinstate custom sign-in page

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -73,13 +73,13 @@ export const authOptions = {
     //     }
     // },
 
-    // pages: {
-        // signIn: '/auth/signin',
+    pages: {
+        signIn: '/auth/signin',
         // signOut: '/', // not needed, we're handling signout silently
         // error: '/auth/error', // Error code passed in query string as ?error=
         // verifyRequest: '/auth/verify-request', // (used for check email message)
         // newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
-    // },
+    },
 }
 
 export default NextAuth(authOptions)
